a7735a1603a03300dab9aaca23c96ef33794b60c
勇 黄 <conan198581@outlook.com>
2019-05-15 17:22:23 +0800

    添加项目文件。

---
+++ MVC05_TagHelper/Services/CinemaService.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Models;
+
+namespace MVC05_TagHelper.Services
+{
+    public class CinemaService : ICinemaService
+    {
+        private readonly List<Cinema> _cinemas = new List<Cinema>();
+
+        public CinemaService()
+        {
+            _cinemas.Add(new Cinema
+            {
+                Id = 1,
+                Name = "City Cinema",
+                Location = "Road ABC, No.123",
+                Capacity = 1000
+            });
+            _cinemas.Add(new Cinema
+            {
+                Id = 2,
+                Name = "Fly Cinema",
+                Location = "Road Hello, No.1024",
+                Capacity = 500
+            });
+        }
+
+        public Task<IEnumerable<Cinema>> GetllAllAsync()
+        {
+            return Task.Run(() => _cinemas.AsEnumerable());
+        }
+
+        public Task<Cinema> GetByIdAsync(int id)
+        {
+            return Task.Run(() => _cinemas.FirstOrDefault(x => x.Id == id));
+        }
+
+        public Task AddAsync(Cinema model)
+        {
+            var maxId = _cinemas.Max(x => x.Id);
+            model.Id = maxId + 1;
+            _cinemas.Add(model);
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ MVC05_TagHelper/Services/ICinemaService.cs
@@ -0,0 +1,15 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC05_TagHelper.Services
+{
+    public interface ICinemaService
+    {
+        Task<IEnumerable<Cinema>> GetllAllAsync();
+        Task<Cinema> GetByIdAsync(int id);
+        Task AddAsync(Cinema model);
+    }
+}
